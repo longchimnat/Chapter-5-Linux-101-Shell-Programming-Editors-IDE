da546dae1019f61c20bc9c4bb1e6df48ab6073c7
Dr@90n_8@ll <phl2007yt@gmail.com>
2026-07-17 21:06:33 +0700

    Update baocao.sh

--- baocao.sh
+++ baocao.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# Bao cao he thong don gian
+HOTEN="Nguyen Van A"
+MSSV="SV001"
+OUT="ketqua.txt"
 
+echo "===== BAO CAO HE THONG =====" > $OUT
+echo "Ho ten : $HOTEN"            >> $OUT
+echo "MSSV   : $MSSV"             >> $OUT
+echo "Thoi gian: `date`"          >> $OUT
+echo "Nguoi dung: `whoami`"       >> $OUT
+echo "Thu muc  : `pwd`"           >> $OUT
+echo "--- Danh sach file ---"     >> $OUT
+ls -l                             >> $OUT
+echo "So luong file: `ls | wc -l`" >> $OUT
